af7753b2d34449d15bed4cfbad7cd3ece3ea3924
Evgeniy Tikhomolov <tikhomolov@bk.ru>
2023-12-12 22:53:07 +0500

    Add tests for methods

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# qa_python
+1. test_add_new_book_add_two_books Поправлен прекод теста на добавление книг
+2. test_add_new_book_more_than_40symbols Проверка добавления книги больше 40символов
+3. test_set_book_genre_correct_genre Проверка присвоения жанра книге
+3. test_get_books_with_specific_genre Проверка получения списка книг по жанру
+4. test_get_books_genre Проверка получения словаря books_genre
+5. test_get_books_for_children Проверка получения книг без возрастного рейтинга
+6. test_add_book_in_favorites Провекра добавления в избранное
+7. test_delete_book_from_favorites Проверка удаления из избранного
+8. test_get_list_of_favorites_books Проверка получения списка избранного
+ 
+ 
+  
+
--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
-from main import BooksCollector
+import pytest
 
+from main import BooksCollector
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
 # обязательно указывать префикс Test
 class TestBooksCollector:
@@ -18,7 +19,68 @@ class TestBooksCollector:
 
         # проверяем, что добавилось именно две
         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+        assert len(collector.get_books_genre()) == 2
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_add_new_book_more_than_40symbols(self):
+        collector = BooksCollector()
+        collector.add_new_book('Незнайка на лунеНезнайка на лунеНезнайка ')
+
+        assert collector.books_genre == {}
+    @pytest.mark.parametrize(
+        'name, genre',
+        [
+            ['Незнайка на луне','Мультфильмы'],
+            ['Кошмар на улице вязов', 'Ужасы']
+        ]
+    )
+    def test_set_book_genre_correct_genre(self, name, genre):
+        collector = BooksCollector()
+        collector.add_new_book(name)
+        collector.set_book_genre(name, genre)
+
+        assert collector.get_book_genre(name) == genre
+
+    def test_get_books_with_specific_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book('Незнайка на луне')
+        collector.set_book_genre('Незнайка на луне', 'Мультфильмы')
+
+        assert collector.get_books_with_specific_genre('Мультфильмы') == ['Незнайка на луне']
+
+    def test_get_books_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book('Незнайка на луне')
+
+        assert collector.get_books_genre() == {'Незнайка на луне': ''}
+
+    def test_get_books_for_children(self):
+        collector = BooksCollector()
+        collector.add_new_book('Незнайка на луне')
+        collector.set_book_genre('Незнайка на луне', 'Мультфильмы')
+
+        collector.add_new_book('Кошмар на улице вязов')
+        collector.set_book_genre('Кошмар на улице вязов', 'Ужасы')
+
+        assert collector.get_books_for_children() == ['Незнайка на луне']
+
+    def test_add_book_in_favorites(self):
+        collector = BooksCollector()
+        collector.add_new_book('Незнайка на луне')
+        collector.add_book_in_favorites('Незнайка на луне')
+
+        assert collector.favorites == ['Незнайка на луне']
+
+    def test_delete_book_from_favorites(self):
+        collector = BooksCollector()
+        collector.add_new_book('Незнайка на луне')
+        collector.delete_book_from_favorites('Незнайка на луне')
+
+        assert collector.favorites == []
+
+    def test_get_list_of_favorites_books(self):
+        collector = BooksCollector()
+        collector.add_new_book('Незнайка на луне')
+        collector.add_book_in_favorites('Незнайка на луне')
+
+        assert collector.get_list_of_favorites_books() == ['Незнайка на луне']
+
